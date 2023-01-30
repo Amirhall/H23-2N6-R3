1e4e151d0133fc617041eba9f52287498aa9007d
Hal Houssine Amir <2284873@cegepmontpetit.ca>
2023-01-30 08:50:38 -0500

    Initialisation

---
+++ Ex1 String.py
@@ -0,0 +1,58 @@
+
+# Pour chaque question ci-dessous, faites le code demandé puis imprimez toujours 80*'_' afin de séparer les questions.      #
+# Imprimez le résultat en utilisant f-string pour afficher le numéro de la question et les infos demandées                  #
+
+
+
+# Q1                                                                                                                   #
+# Vous avez ici une liste de différents processeurs            
+# Vous pouvez aussi mettre un autre modèle de carte graphique si vous voulez.#
+# imprimez la liste                                                                                                    #
+# Changez la liste en une chaine de caractères avec les cartes graphiques séparées par des virgules
+
+
+
+
+
+
+#Q2                                                                                                                   #
+#  Voici une chaine de caractères qui ressemble à une ligne de données que vous auriez extraite d'un fichier text     #
+ligne_donnees = " AMD Ryzen 9 5900X ;  AMD Ryzen 7 5800X3 ;  Intel Core i9 12900 K    "                               #
+#  Vous devez dans un premier temps séparer les données sur le caractère qui les séparent
+#  Vous voulez ensuite avoir une liste de chacun des processeurs sans les espaces avant et après chaque processeur processeurs                                                                                            #
+#  Imprimez la liste maintenant                                                                    #
+
+
+
+
+
+
+
+
+
+# Q3                                                                                                                   #
+# Voici un nom de fichier dont chaque partie est séparée par un _                                                      #
+nom_fichier_et_extension = "Python_Rencontre 3_Approfondissement str.docx"                                             
+# Séparez nom_fichier_et_extension sur le '.' et gardez les parties dans des sous-chaines séparées                     #
+# Séparez le nom de fichier de façon à récupérer le nom du cours, la rencontre et le sujet de la rencontre             #
+# Imprimez le nom du cours, la rencontre et le sujet de la rencontre                                                   #
+
+
+
+
+
+
+
+
+#Q4
+#  remplir un string pour qu'il fasse 2 caractères de long avec .zfill
+#  Au départ, vous avez une chaine qui contient des nombres
+str_nombres = "1,5,10,15,20,25"
+#  transformez cette chaine en une liste
+#  triez cette liste en nombres croissants tout en conservant la liste de nombres originale (avec sorted)
+# Imprimez la liste. Qu'est-ce qui est étonnant?
+# Pour pouvoir avoir la liste de string dans un ordre croissant numérique il faut remplacer les valeurs pour qu'ils soient tous 2 de long
+# Un fois cela-fait, imprimez la liste en ordre croissant de nouveau
+
+
+
---
+++ Ex2_OS.py
@@ -0,0 +1,49 @@
+import os
+from datetime import datetime
+
+# Q1  imprimez le répertoire courant
+
+
+print(80*'_' )
+# Q2   ATTENTION sous windows, la variable d'environnement équivalente à HOME de Linux est USERPROFILE
+#      MALGRÉ CELA, on va toujours parler de votre HOME
+#   Imprimez la variable d'environnement USERPROFILE
+
+
+
+print(80*'_' )
+# Q3 Déplacez-vous sur le répertoire 'Desktop' de votre HOME                            #
+# Et imprimez le répertoire courant                                                     #
+
+
+print(80*'_' )
+# Q4   Imprimez la liste des répertoires et des fichiers qu'il y a dans votre Desktop   #
+
+
+
+print(80*'_' )
+# Q5   Créez un répertoire OS-DemoQ5                                               #
+# Et réimprimez les répertoires et les fichiers dans votre Desktop                      #
+
+
+
+print(80*'_' )
+# Q6   création des répertoires  OS-DemoQ6/Subdir1 avec une seule instruction
+# Et réimprimez les répertoires et les fichiers dans votre Desktop                      #
+
+
+
+print(80*'_' )
+#Q7   Renommez le répertoire Subdir1 pour qu'il s'appelle Sous_repertoire
+
+
+
+print(80*'_' )
+# Q8   suppression du répertoire OS-DemoQ6 et de son contenu
+# Et réimprimez les répertoires et les fichiers dans votre bureau#
+
+
+
+
+
+print(80*'_' )
